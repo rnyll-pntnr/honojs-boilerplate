253f85c138c5af54ebd1a44da4771962578ec6d7
Ranyll <ranyllpuntanar@gmail.com>
2025-05-21 07:56:37 +0400

    feat: make db async

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { logger } from 'hono/logger'
 import { cors } from 'hono/cors'
 import { Hono } from 'hono'
 import { ZodError, type ZodIssue } from 'zod'
-import { defaultRoutes } from './routes/index'
+import { defaultRoutes } from './routes'
 import { dbConfig } from './config/database'
 import { PinoLogger } from './utils/logger'
 
@@ -13,8 +13,6 @@ export const app: Hono = new Hono()
 
 app.use(logger())
 app.use(cors())
-
-dbConfig(pinoLogger)
 
 app.onError((error, c) => {
   if (error instanceof ZodError) {
@@ -64,13 +62,16 @@ defaultRoutes.forEach((route) => {
   app.route(route.path, route.route)
 })
 
+const server = serve({
+  fetch: app.fetch,
+  port: parseInt(`${process.env.PORT}`) ?? 3000
+}, async (info) => {
+  await dbConfig(pinoLogger)
+  pinoLogger.info(`Server is running on PORT:${info.port}`)
+})
+
 export default {
-  app: serve({
-    fetch: app.fetch,
-    port: parseInt(`${process.env.PORT}`) ?? 3000
-  }, (info) => {
-    pinoLogger.info(`Server is running on PORT:${info.port}`)
-  })
+  app: server
 }
 
 
